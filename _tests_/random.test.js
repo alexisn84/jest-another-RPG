4c0f8457746d66d0b809f2ed9b342c18b59f379c
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2021-12-03 14:26:16 -0500

    fix jest function in json package

--- _tests_/random.test.js
+++ _tests_/random.test.js
@@ -1,7 +1,6 @@
-module.exports = function(val1, val2) {
-    if (val1 === val2) {
-        return true;
-        } else {
-        return false;
-    }
-};
+const checkIfEqual = require('../lib/random.js');
+
+test('checks if 10 is equal to 10', () => {
+    expect(checkIfEqual(10, 10)).toBe(true);
+});
+
